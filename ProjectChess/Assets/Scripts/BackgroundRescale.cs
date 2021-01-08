237c95bd4d8c2f7b3b2ffbc1ddf66dc059011777
Mikołaj <mikolajtechhnika@gmail.com>
2021-01-08 11:19:48 +0100

    Added Main Menu

---
+++ ProjectChess/Assets/Scripts/BackgroundRescale.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BackgroundRescale : MonoBehaviour
+{
+    Image bgImage;
+    RectTransform rt;
+    float aspectRatio;
+
+    void Start()
+    {
+        bgImage = GetComponent<Image>();
+        rt = bgImage.rectTransform;
+        aspectRatio = bgImage.sprite.bounds.size.x / bgImage.sprite.bounds.size.y;
+    }
+
+    void Update()
+    {
+        if (!rt)
+        {
+            return;
+        }
+
+        if(Screen.height * aspectRatio >= Screen.width)
+        {
+            rt.sizeDelta = new Vector2(Screen.height * aspectRatio, Screen.height);
+        }
+        else
+        {
+            rt.sizeDelta = new Vector2(Screen.width, Screen.width / aspectRatio);
+        }
+    }
+}
---
+++ ProjectChess/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+    void Start()
+    {
+        MainMenuButton();
+    }
+
+    public void PlayButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(""); // Load Main level
+    }
+
+    public void MainMenuButton()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    public void OptionsButton()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
