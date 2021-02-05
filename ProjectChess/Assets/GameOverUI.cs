44da2cef03b4c2ff5d7746157d83039211f7d785
Mikołaj <mikolajtechhnika@gmail.com>
2021-02-05 03:08:01 +0100

    Menu pauzy, coś nie do końca działa. Metody na pokazanie okien game over i game won do wywołania przy odpowiadających eventach

---
+++ ProjectChess/Assets/GameOverUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverUI : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("APP QUIT");
+        Application.Quit();
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
